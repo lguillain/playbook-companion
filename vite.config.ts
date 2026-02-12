444eea39ca598a82e6b88a7fcedb5ff69f17d88b
Leonore <leonore.guillain@bluewin.ch>
2026-02-12 16:32:01 +0100

    pages specific logic

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/playbook-companion/",
+  base: process.env.GITHUB_PAGES ? "/playbook-companion/" : "/",
   server: {
     host: "::",
     port: 8080,
